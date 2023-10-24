ab0327e18e95ea6d07d1d6d947d2c5a51e8c5e5a
David Perez <deividpa022@gmail.com>
2023-10-23 23:38:47 -0500

    New project implement JPA initialized

---
+++ JPA-1/ProductsSupplier/src/productssupplier/ProductsSupplier.java
@@ -0,0 +1,13 @@
+package productssupplier;
+
+/**
+ *
+ * @author David Pérez
+ */
+public class ProductsSupplier {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
